6bf3d68ce58b0ef3a5c0e60eccd9f990319c9cd5
Gary Bright <garybright@rocketmail.com>
2024-04-08 22:56:51 -0400

    added requires, pool, and inital prompt for the application

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
+const express = require('express');
+const inquirer = require('inquirer');
 
+const {Pool} = require('pg');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const pool = new Pool(
+    {
+        user: 'postgres',
+        password: 'password123',
+        host: 'localhost',
+        database: 'employee_db'
+    },
+
+)
+
+pool.connect();
+
+
+function question(){
+    inquirer
+    .prompt({
+        type: 'list',
+        name: 'inital',
+        message: 'Please choose from the following:',
+        choices: ['View all departments', 'View all roles', 'View all employees',
+        'Add a department', 'Add a role', 'Add a employee', 'Update an employee role' ]
+    })
+}
